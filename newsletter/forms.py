cb6f988de8435d8d2e8aa9f358a285f6c28226e7
ArtemHerasymenko <agerasimencko34@gmail.com>
2018-09-12 11:16:40 +0300

    after 12th lesson

---
+++ newsletter/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+
+from .models import SignUp
+
+class SignUpForm(forms.ModelForm):
+	class Meta:
+		model = SignUp
+		fields = ['full_name','email']
+
+	def clean_email(self):
+		email = self.cleaned_data.get('email')
+		if not 'edu' in email:
+			raise forms.ValidationError("Please use .EDU email")
+		return email
