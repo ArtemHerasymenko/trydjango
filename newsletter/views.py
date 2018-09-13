fb3ef486f9a42f805e2aa86632a13ac51ec76c22
ArtemHerasymenko <agerasimencko34@gmail.com>
2018-09-13 10:14:53 +0300

    after 14th lesson

--- newsletter/views.py
+++ newsletter/views.py
@@ -1,6 +1,38 @@
 from django.shortcuts import render
+from .forms import SignUpForm
 
 # Create your views here.
 
 def home(request):
-	return render(request, "home.html", {})
+	title = "My title"
+	name = "Welcome"
+
+	if(request.user.is_authenticated()):
+		name = request.user
+
+	form = SignUpForm(request.POST or None)
+	
+	context = {
+		"title" : title,
+		"name" : name,
+		"form" : form
+	}
+
+	if form.is_valid():
+		print("Form Is Valid")
+		instance = form.save()
+		print("Instance = ",instance)
+		print(instance.email)
+		print(instance.timestamp)
+		context = {
+			"title" : "Thank you"
+		}
+	
+
+	print("method=",request.method)
+	print("POST=",request.POST)
+	print("GET=",request.GET)
+	
+
+
+	return render(request, "home.html", context)
